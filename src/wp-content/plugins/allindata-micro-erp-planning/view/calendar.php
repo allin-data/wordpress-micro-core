9b15fcac82ed736a1b60c4cfd8ff4532c9bb04eb
Florian Horn <florian.horn@all-in-data.de>
2019-04-09 21:20:39 +0200

    Updated theme jquery version

--- src/wp-content/plugins/allindata-micro-erp-planning/view/calendar.php
+++ src/wp-content/plugins/allindata-micro-erp-planning/view/calendar.php
@@ -9,17 +9,52 @@ Copyright (C) 2019 All.In Data GmbH
 <div id="calendar" style="height: 800px;"></div>
 
 <script>
-    jQuery(document).ready(function () {
-        jQuery('#calendar').tuiCalendar({
+    $(document).ready(function () {
+        var Calendar = tui.Calendar;
+        var calendar = new Calendar('#calendar', {
             defaultView: 'month',
             taskView: true,
             template: {
-                monthGridHeader: function (model) {
+                monthGridHeader: function(model) {
                     var date = new Date(model.date);
                     var template = '<span class="tui-full-calendar-weekday-grid-date">' + date.getDate() + '</span>';
                     return template;
                 }
             }
         });
+
+        $('#calendar').tuiCalendar({
+            defaultView: 'month',
+            taskView: true,
+            template: {
+                monthGridHeader: function(model) {
+                    var date = new Date(model.date);
+                    var template = '<span class="tui-full-calendar-weekday-grid-date">' + date.getDate() + '</span>';
+                    return template;
+                }
+            }
+        });
+
+        calendar.createSchedules([
+            {
+                id: '1',
+                calendarId: '1',
+                title: 'my schedule',
+                category: 'time',
+                dueDateClass: '',
+                start: '2019-04-18T22:30:00+09:00',
+                end: '2019-04-19T02:30:00+09:00'
+            },
+            {
+                id: '2',
+                calendarId: '1',
+                title: 'second schedule',
+                category: 'time',
+                dueDateClass: '',
+                start: '2019-04-18T17:30:00+09:00',
+                end: '2019-04-19T17:31:00+09:00',
+                isReadOnly: true    // schedule is read-only
+            }
+        ]);
     });
 </script>
