169473a84d11d5a5b5b84956477449cda734b571
Florian Horn <florian.horn@all-in-data.de>
2019-04-09 21:33:32 +0200

    cleanup calendar js

--- src/wp-content/plugins/allindata-micro-erp-planning/view/calendar.php
+++ src/wp-content/plugins/allindata-micro-erp-planning/view/calendar.php
@@ -23,18 +23,6 @@ Copyright (C) 2019 All.In Data GmbH
             }
         });
 
-        $('#calendar').tuiCalendar({
-            defaultView: 'month',
-            taskView: true,
-            template: {
-                monthGridHeader: function(model) {
-                    var date = new Date(model.date);
-                    var template = '<span class="tui-full-calendar-weekday-grid-date">' + date.getDate() + '</span>';
-                    return template;
-                }
-            }
-        });
-
         calendar.createSchedules([
             {
                 id: '1',
